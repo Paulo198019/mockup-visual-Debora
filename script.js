82f555c93eb09b6893be01fd7d7b216860b51576
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-11-21 21:14:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('a[href^="#"]').forEach(anchor=>{anchor.addEventListener('click',e=>{e.preventDefault();document.querySelector(anchor.getAttribute('href')).scrollIntoView({behavior:'smooth'});});});});
