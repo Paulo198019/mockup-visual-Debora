04d23692647e1f31c314b4990239a97168a0f100
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-11-21 23:00:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{const form=document.querySelector('form');form.addEventListener('submit',e=>{e.preventDefault();alert('Mensagem enviada com sucesso!');form.reset();});});
